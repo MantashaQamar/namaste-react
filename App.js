a2f6517a9c8f0d666381a567c5d8256fb877d92c
@mantasha_qamar <mantasha_qamar@epam.com>
2024-12-11 12:09:29 +0530

    epi-1

---
+++ App.js
@@ -0,0 +1,40 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hemlo from React");
+//{} above is the place where we'll give attributes to our tags, suupose 
+//the h1 tag needs to have an id then we {id: "heading"}
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+/**
+ * <div id = "parent">
+ *     <div id ="child">
+ *     <h1>I'm h1 tag</h1>
+ *     <h2>I'm h2 tag- Sibling </h2>
+ *     </div>
+ *     <div id ="child2">
+ *     <h1>I'm h1 tag</h1>
+ *     <h2>I'm h2 tag- Sibling </h2>
+ *     </div>
+ * </div>
+ * 
+ */
+
+const parent= [React.createElement("div", {id: "parent"},
+    React.createElement("div", {id:"child"}, 
+        // React.createElement("h1", {}, "I'm h1 tag") //Single tag at child level
+       [ React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 - sibling tag of h1") ]//sibling tags at same level
+        //if you want to have siblings ie, if you wangt to have 2 h1 tags together, you are supposed to write it as an array [...]
+    )
+),
+    React.createElement("div" ,{id:"child2"}, 
+        [ React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 - sibling tag of h1") ] 
+    )
+];
+
+
+// const heading = React.createElement("h1 ", {id: "heading"}, "Hemlo from React");
+// console.log(heading); // object
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(parent);
