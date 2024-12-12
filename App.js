388ed1de8f43c9eda96916204beb4ee2a8a273bf
@mantasha_qamar <mantasha_qamar@epam.com>
2024-12-12 14:11:38 +0530

    added more info

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // const heading = React.createElement("h1", {id: "heading"}, "Hemlo from React");
-//{} above is the place where we'll give attributes to our tags, suupose 
+//{} above is the place where we'll give attributes to our tags, suupose
 //the h1 tag needs to have an id then we {id: "heading"}
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
@@ -14,27 +14,54 @@
  *     <h2>I'm h2 tag- Sibling </h2>
  *     </div>
  * </div>
- * 
+ *
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent= [React.createElement("div", {id: "parent"},
-    React.createElement("div", {id:"child"}, 
-        // React.createElement("h1", {}, "I'm h1 tag") //Single tag at child level
-       [ React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 - sibling tag of h1") ]//sibling tags at same level
-        //if you want to have siblings ie, if you wangt to have 2 h1 tags together, you are supposed to write it as an array [...]
-    )
-),
-    React.createElement("div" ,{id:"child2"}, 
-        [ React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 - sibling tag of h1") ] 
-    )
-];
+// const parent = [
+//   React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement(
+//       "div",
+//       { id: "child" },
+      //-- React.createElement("h1", {}, "I'm h1 tag") //Single tag at child level
+    //   [
+    //     React.createElement("h1", { id: "heading" }, "I'm h1 tag"),
+    //     React.createElement("h2", {}, "I'm h2 - sibling tag of h1"),
+    //   ] //--sibling tags at same level
+      //--if you want to have siblings ie, if you wangt to have 2 h1 tags together, you are supposed to write it as an array [...]
+//     )
+//   ),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I'm h1 tag"),
+//     React.createElement("h2", {}, "I'm h2 - sibling tag of h1"),
+//   ]),
+// ];
 
+// --const heading = React.createElement("h1 ", {id: "heading"}, "Hemlo from React");
+// --console.log(heading); // object
+// console.log(parent);
 
-// const heading = React.createElement("h1 ", {id: "heading"}, "Hemlo from React");
-// console.log(heading); // object
-console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// --root.render(heading);
+// root.render(parent);
+
+const jsxHeading = (
+    <h1 className="head" tabIndex = "5">
+        Namaste React using JSX 🤖
+    </h1>
+);
+
+// console.log(jsxHeading);
+
+//------REACT COMPONENTS = Functional based components--------//
+const HeadingComponent = () => {
+    return <h1>Namaste React Functional Component</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(parent);
+root.render(jsxHeading);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+...
 # Namaste React 
+
+...
+# Used Parcel - Web Pack  
+-Dev Build
+-Local Server
+-Hot Module Replacement[HMR]
+-PARCEL Uses File watching Algo - written in C++
+-Faster builds bcoz of Caching
+-Image optimization
+-Minification of files
+-bundling of the files
+-Compress files
+-Parcel uses Consitent hashing
+-Code splitting
+-Differenctial Bundling - to support older Browsers
+-Diagnostics
+-Error Handling
+-HTTPS hosting features
+-Tree shaking - Remove unused code
